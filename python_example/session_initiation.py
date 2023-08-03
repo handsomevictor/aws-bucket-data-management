6aaa14091ab48ab4e61b8660a243122e04927319
Handsome Victor <857730556@qq.com>
2023-08-03 14:38:03 +0100

    Update session_initiation.py

--- python_example/session_initiation.py
+++ python_example/session_initiation.py
@@ -1,20 +1,17 @@
-import os
 import boto3
-import gzip
-from concurrent.futures import ThreadPoolExecutor
-from itertools import repeat
-from tqdm import tqdm
-import shutil
 
 session = boto3.Session()
 s3 = boto3.resource('s3',
-                    aws_secret_access_key=session.get_credentials().get_frozen_credentials().secret_key,
-                    aws_access_key_id=session.get_credentials().get_frozen_credentials().access_key
+                    aws_secret_access_key="Your secret key",
+                    aws_access_key_id="Your access key"
                     )
 s3_cli = boto3.client('s3',
-                      aws_secret_access_key=session.get_credentials().get_frozen_credentials().secret_key,
-                      aws_access_key_id=session.get_credentials().get_frozen_credentials().access_key
+                      aws_secret_access_key="Your secret key",
+                      aws_access_key_id="Your access key"
                       )
 
 # Now the session is initiated, you can use the s3 and s3_cli objects to interact with S3 like downloading/uploading
 # files, getting the list of files that are in a bucket, etc.
+
+# This is not working for MFA, for MFA enabled initiation, there will be a separate script soon to be uploaded
+
